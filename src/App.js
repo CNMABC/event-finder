bb8eb1e7a1f47aeed42e72950ddfc2c7bed84f36
Arjun Doel <arjundoel@gmail.com>
2021-06-17 18:59:38 +0100

    setup start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-function App() {
-  return <h1>Hello World</h1>
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  )
 }
 
 export default App
+
